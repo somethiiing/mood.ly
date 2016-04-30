3567968586258bb950a67adaff8267bc9abf1822
Arun Vasudevan <arunv83@gmail.com>
2016-04-30 11:55:27 -0700

    Create Local Strategy for users to create account and login locally. Passport initialization and session have been created but generateHash function needs to be written.

--- server/config/passport.js
+++ server/config/passport.js
@@ -14,11 +14,56 @@ module.exports = function(passport) {
   });
 
   //PASSPORT SESSION
-  app.use(session({ secret: 'shhsecret' }));  
+  app.use(session({
+    secret: 'shhsecret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { path: '/', httpOnly: true, secure: false, maxAge: null }
+    }));
+
+  //PASSPORT INITIALIZE & SESSION
   app.use(passport.initialize());  
-  app.use(passport.session());  
+  app.use(passport.session());
+
+  //FLASH MESSAGES  
   app.use(flash());
 
-
+  //LOCAL STRATEGY
+  passport.user('local-signup', new LocalStrategy({
+    username: 'email',
+    password: 'password',
+    passReqToCallback: true,
+  },
+  function(req, email, password, done) {
+      //SEARCH DATABASE FOR USER
+      User.findOne({ where: { 'local.email': email } })
+      .then(function(user) {
+        console.log('Found user ', user);
+        // if (err) {
+        //   return done(err);
+        // }
+        // if (user) {
+        //   return done(null, false, req.flash('signupMessage', 'That email is already in use.'));
+        if (user) {
+          done(null, user);
+        } else {
+          //CREATE NEW USER
+          User.create({
+            username: email,
+            password: generateHash(password), //NEED TO WRITE METHOD FOR GENERATEHASH
+          })
+          .then(function(newUser) {
+            console.log('user created ', newUser);
+            done(null, newUser);
+          }
+          .catch(function(err) {
+            console.log('Error creating a new user! ', err);
+          }));
+        }
+      })
+      .catch(function(err) {
+        console.log('Cannot find user! ', err);
+      });
+  }));
 };
 
