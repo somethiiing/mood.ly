317628cdb53d273fcc6cf894c8ebf469dc223e78
clardizabal <chris.lardizabal@gmail.com>
2016-05-03 12:15:05 -0700

    change to es6 syntax

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-var db = require('./db/db.js');
-var User = require('./models/userModel.js');
-var Quote = require('./models/quoteModel.js');
-var Mood = require('./models/moodModel.js');
+import db from './db/db';
+import User from './models/userModel';
+import Quote from './models/quoteModel';
+import Mood from './models/moodModel';
 
 Mood.belongsToMany(User, {through: 'UserMood'});
 User.belongsToMany(Mood, {through: 'UserMood'});
