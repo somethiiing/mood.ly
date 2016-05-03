2418eb5c2c4cb82174453335d6e51ed568273f8a
clardizabal <chris.lardizabal@gmail.com>
2016-05-03 16:39:17 -0700

    call wiki api and render result

--- client/app/app.js
+++ client/app/app.js
@@ -1,27 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import wiki from './services/wiki.js';
 import $ from 'jquery';
-
-const menuData = [
-  { 
-    mood: 'Happy',
-    choice: 0,
-    quote: 'The best way to find yourself is to lose yourself in the service of others.',
-    image: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2014/09/best-wallpapers-3.jpg',
-  },
-  {
-    mood: 'sad',
-    choice: 1,
-    quote: 'Do the best you can, and don\'t take life too serious.',
-    image: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2014/09/best-wallpapers-2-610x343.jpg',
-  },
-  {
-    mood: 'funny',
-    choice: 2,
-    quote: 'It\'s a funny thing about life; if you refuse to accept anything but the best, you very often get it.',
-    image: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2014/09/best-wallpapers-7.jpg',
-  },
-];
+import Nav from './components/Nav';
 
 class App extends Component {
   constructor(props) {
@@ -33,64 +13,36 @@ class App extends Component {
 
     //set default state
     this.state = {
-      currMood: menuData[0].mood,
-      currQuote: menuData[0].quote,
+      currMood: '',
+      currQuote: '',
+      currentSearch: '',
+      requestMadeAfterInterval: false
     };
   }
 
-    onChoiceClick (event) {   
-      event.preventDefault();
-      console.log(event);
-      var that = this;
-      var data;
-      console.log('event.target.value ', event.target.value);
-      wiki("happy", function (res) {
-        data = res.body;
-        // console.log(data);
-        console.log('this ', this);
-        console.log('that', that);
-        // console.log(that.state.currQuote);
-        // this.state.currQuote = data[0];
-        that.setState({
-          currMood: "hello",
-          currQuote: data[0],
-
+    handleSearchButtonClick() {
+      var self = this;
+      let query = this.state.currentSearch;
+      console.log(query);
+      wiki(query, function (res) {
+        console.log(res.body[0]);
+        self.setState({
+          currMood: query,
+          currQuote: res.body[0]
         });
-
-        return false;
       });
-    }
-    
-    // onChoiceClick (choice) {
-    //   wiki(menuData[choice.target.value].mood, (res) => {
-    //     this.setState({
-    //       currMood: choice,
-    //       currChoice: menuData[choice.target.value],
-    //       currQuote: res[0],
-    //       currImg: menuData[choice.target.value].image
-    //     });
-    //   })
-    // }
-
-    onType (e) {
-      const self = this;
-      this.setState({
-        currMood: e,
-      });
-      // console.log("eeeee", e)
-      // console.log(this.state);
+      this.setState({requestMadeAfterInterval: true});
     }
 
+    handleSearchChange(event) {
+      this.setState({currentSearch: event.target.value});
+    }
 
     render() {
       return (
         <div>
           <h1>mood.ly</h1>
-          <form>
-            <input type="text" placeholder="How are you feeling?" defaultValue="Happy" onChange={this.onType.bind(this)}/> 
-            <br />
-            <input className="moodsubmit" type="submit" value="Submit!" onClick={this.onChoiceClick.bind(this)}/>
-          </form>
+          <Nav handleSearchChange={this.handleSearchChange.bind(this)} handleSearchButtonClick={this.handleSearchButtonClick.bind(this)}/>
           <div className="moodly-content">
             <span className="quote-title"><h2>{this.state.currQuote}</h2></span>
           </div>
@@ -98,6 +50,5 @@ class App extends Component {
       );
     }
 }
-            // <Quote quoteList={this.state.quotes} onChange={this.onChoiceClick.bind(this)} />
 
 export default App;
---
+++ client/app/components/Search.js
@@ -0,0 +1,10 @@
+import React, { PropTypes, Component } from 'react';
+
+export default (props) => (
+  <div className="search-bar form-inline">
+    <input onChange={props.handleSearchChange} className="form-control" type="text" placeholder="How are you feeling?"/>
+    <button onClick={props.handleSearchButtonClick} value="Submit!">
+      <span className="glyphicon glyphicon-search"></span>
+    </button>
+  </div> 
+);
