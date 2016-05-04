2b66d657b36f6a3e747cc1e7c2f77d17b046ae95
Kim Curran <currankim7@gmail.com>
2016-05-03 17:03:00 -0700

    Center search bar

--- client/app/components/Search.js
+++ client/app/components/Search.js
@@ -2,9 +2,11 @@ import React, { PropTypes, Component } from 'react';
 
 export default (props) => (
   <div className="search-bar form-inline">
-    <input onChange={props.handleSearchChange} className="form-control" type="text" placeholder="How are you feeling?"/>
-    <button onClick={props.handleSearchButtonClick} value="Submit!">
+    <div className="form-group">
+      <input onChange={props.handleSearchChange} className="form-control" type="text" placeholder="How are you feeling?"/>
+      <button onClick={props.handleSearchButtonClick} value="Submit!">
       <span className="glyphicon glyphicon-search"></span>
-    </button>
+      </button>
+    </div>
   </div> 
 );
