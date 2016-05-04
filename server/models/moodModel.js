cd500adf702a4228d2ed3658f9a57bbf24974a2c
clardizabal <chris.lardizabal@gmail.com>
2016-05-04 10:01:08 -0700

    save mood dates in database

--- server/models/moodModel.js
+++ server/models/moodModel.js
@@ -4,7 +4,7 @@ import db from '../db/db';
 export default db.define('Mood',
   {
     name: Sequelize.STRING,
-    timedate: Sequelize.STRING
+    timedate: Sequelize.DATE
   },
   {
     freezeTableName: true
