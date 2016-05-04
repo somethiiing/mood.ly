ea777ef3043f3ef4065fb42f538193bd211b23db
clardizabal <chris.lardizabal@gmail.com>
2016-05-04 15:50:20 -0700

    disable sequelize logging

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,8 @@ var password = require('../config/mysqlsetup.js');
 var db = new Sequelize('moodb', 'root', password, {
   define: {
     timestamps: false // true by default
-  }
+  },
+  logging: false
 });
 
 module.exports = db;
