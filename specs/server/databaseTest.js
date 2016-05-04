c6f3516b3b1eddbbfd6d91eed14e7264c6abd728
Arun Vasudevan <arunv83@gmail.com>
2016-05-03 17:10:19 -0700

    Add Mocha tests for databaseTest.js

---
+++ specs/server/databaseTest.js
@@ -0,0 +1,23 @@
+import chai from 'chai';
+import request from 'request';
+import User from '../../server/models/userModel';
+
+const expect = chai.expect;
+
+describe('Database', () => {
+
+  it('should return all users who have the given mood', (done) => {
+    expect().to.equal(); //TODO: FILL OUT EXPECT AND EQUAL
+    done();
+  });
+
+  it('should return all quotes for each user', (done) => {
+    expect().to.equal(); //TODO: FILL OUT EXPECT AND EQUAL
+    done();
+  });
+
+  it('should retrieve all users in the database', (done) => {
+    expect().to.equal(); //TODO: FILL OUT EXPECT AND EQUAL
+    done();
+  });
+});
