db08745265ff3378d658a782c1bb8e8b063562b3
Arun Vasudevan <arunv83@gmail.com>
2016-05-06 12:12:28 -0700

    Update eslint on app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import wiki from './services/wiki.js';
-import $ from 'jquery';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Login from './components/Login';
@@ -23,60 +22,67 @@ class App extends Component {
       showSignUp: false,
     };
   }
-    onSignUpClick() {
-      console.log('signup function fired');
-      this.setState({
-        showSignUp: !this.state.showSignUp,
-      });
-    }
 
-    onLoginClick() {
-      console.log('login function fired');
-      this.setState({
-        showLogin: !this.state.showLogin,
-      });
-    }
+  onSignUpClick() {
+    this.setState({
+      showSignUp: !this.state.showSignUp,
+    });
+    this.onSignUpClick = this.onSignUpClick.bind(this);
+  }
 
-    handleSearchButtonClick() {
-      var self = this;
-      let query = this.state.currentSearch;
-      console.log(query);
-      wiki(query, function (res) {
-       var randomIndex = Math.floor((Math.random()*res.length) + 1) ;
-       console.log(randomIndex); 
-        self.setState({
-          currMood: query,
-          currQuote: res[randomIndex]
-        });
-      });
-    }
+  onLoginClick() {
+    this.setState({
+      showLogin: !this.state.showLogin,
+    });
+    this.onLoginClick = this.onLoginClick.bind(this);
+  }
 
-    handleSearchChange(event) {
-      this.setState({
-        currentSearch: event.target.value
+  handleSearchButtonClick() {
+    const self = this;
+    const query = this.state.currentSearch;
+    wiki(query, (res) => {
+      const randomIndex = Math.floor((Math.random() * res.length) + 1);
+      self.setState({
+        currMood: query,
+        currQuote: res[randomIndex],
       });
-    }
+    });
+    this.handleSearchButtonClick = this.handleSearchButtonClick.bind(this);
+  }
 
-    render() {
-      return (
+  handleSearchChange(event) {
+    this.setState({
+      currentSearch: event.target.value,
+    });
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
         <div>
-          <div>
-            <Header onSignUpClick={this.onSignUpClick.bind(this)} onLoginClick={this.onLoginClick.bind(this)} />
-          </div>
-          <div>
-            {this.state.showLogin ? <Login /> : null}
-          </div>
-          <div>
-            {this.state.showSignUp ? <Signup /> : null}
-          </div>
-          <h1>mood.ly</h1>
-          <Nav handleSearchChange={this.handleSearchChange.bind(this)} handleSearchButtonClick={this.handleSearchButtonClick.bind(this)}/>
-          <div className="moodly-content">
-            <span className="quote-title"><h2>{this.state.currQuote}</h2></span>
-          </div>
+          <Header
+            onSignUpClick={this.onSignUpClick}
+            onLoginClick={this.onLoginClick}
+          />
         </div>
-      );
-    }
+        <div>
+          {this.state.showLogin ? <Login /> : null}
+        </div>
+        <div>
+          {this.state.showSignUp ? <Signup /> : null}
+        </div>
+        <h1>mood.ly</h1>
+        <Nav
+          handleSearchChange={this.handleSearchChange}
+          handleSearchButtonClick={this.handleSearchButtonClick}
+        />
+        <div className="moodly-content">
+          <span className="quote-title"><h2>{this.state.currQuote}</h2></span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
