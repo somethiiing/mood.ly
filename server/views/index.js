7b915c9758e7bde843c3c159949f0396fc71c516
Arun Vasudevan <arunv83@gmail.com>
2016-05-07 17:07:21 -0700

    Update route to style.css in index.js under views folder.

--- server/views/index.js
+++ server/views/index.js
@@ -5,7 +5,7 @@ const renderFullPage = function(html, initialState) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="/client/styles/style.css" />
+        <link rel="stylesheet" href="/client/app/styles/style.css" />
 
         <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
