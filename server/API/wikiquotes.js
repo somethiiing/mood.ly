9a852d1c0ef989da2e585a6fa8bfda082e02acab
Wilson Yu <wilsonweichengyu@gmail.com>
2016-05-07 17:10:59 -0700

    added documentation to wiki api

--- server/API/wikiquotes.js
+++ server/API/wikiquotes.js
@@ -27,20 +27,7 @@ const redirectFailMessages = [
 // inside that key, there is a property called the pageID
 // -1 means the page doesnt exist and redirect can't happen.
 // another number means that the page exists, and has a possibility of redirect
-
-// const getPageID = body => {
-//   for (const key of Object.keys(body.query.pages)) {
-//     const index = Number(key);
-//     if (typeof index === 'number') {
-//       if (index === -1) {
-//         return -1;
-//       }
-//       return body.query.pages[index].pageid;
-//     }
-//   }
-//   return '';
-// };
-
+// getPageID is ONLY responsible for returning the pageID
 const getPageID = body => {
   let index;
   for (const key of Object.keys(body.query.pages)) {
@@ -55,21 +42,20 @@ const getPageID = body => {
   return body.query.pages[index].pageid;
 };
 
-// const getPageID = body => {
-//   const keys = Object.keys(body.query.pages);
-//   console.log(keys);
-//   const pageid = Number(keys[0]);
-//   console.log(pageid);
-//   if (pageid === -1) {
-//     return -1;
-//   }
-//   return body.query.pages[pageid].pageID;
-// };
-
+// redirectCheck finds whether or not there is a redirect involved.
+// wikipedia has automatic redirects such as "happy" to "Happiness"
+// "-1" means the page does NOT exist and a redirect doesn't even happen
+// example: if someone typed in "asdfadfsasf", page would return -1
+// redirectCheck is responsible for setting one of the three "paths" it can go
+// null, redirectKey, or false
 const redirectCheck = (body, pageID) => {
   if (pageID === -1) {
     return null;
   }
+  // because of the way the API returns the data
+  // data formatting is inconsistent between the different pages
+  // example: sometimes it's #redirect, #REDIRECT, or #Redirect
+  // manual parsing to track is necessary
   let redirectKey = body.query.pages[pageID].revisions[0]['*'];
   const lowercaseRedirectCheck = (redirectKey.slice(0, 9)).toLowerCase();
   if (lowercaseRedirectCheck === '#redirect') {
@@ -80,6 +66,15 @@ const redirectCheck = (body, pageID) => {
   return false;
 };
 
+// wikiAPI returns all data in a single string.
+// manual parsing was done here to clean up the data
+// steps:
+// 1. split by <br>
+// 2. quotes started with *, so kept all of those
+// 3. removed special characters
+// 4. deleted more special characters
+// 5. filtered more data into cleanData array
+// 6. return object, with success and body with clean data
 const parseData = (body, pageID) => {
   let rawData = body.query.pages[pageID].revisions[0]['*'];
   const filteredData = [];
@@ -111,6 +106,12 @@ const parseData = (body, pageID) => {
   return { status: 'success', body: cleanData };
 };
 
+// RESTful API call, called asynchronously.
+// data was processed using data stream because it wasn't parsable
+// because of a large number of special characters
+// asynchronous call after data stream calls getData (function chain logic)
+// LINTER has "getData(body, callback)" because function is called before it's defined
+// but since the function is called asynchronously and the hoisting of ES6
 const wikiQuoteCall = (keyword, callback) => {
   const url = `https://en.wikiquote.org/w/api.php?action=query&titles=${keyword}&prop=revisions&rvprop=content&format=json`;
   request.get(url)
@@ -119,13 +120,19 @@ const wikiQuoteCall = (keyword, callback) => {
     response.on('data', chunk => {
       body.push(chunk);
     })
-  .on('end', () => {
-    body = JSON.parse(Buffer.concat(body).toString());
-    getData(body, callback);
-  });
+    .on('end', () => {
+      body = JSON.parse(Buffer.concat(body).toString());
+      getData(body, callback);
+    });
   });
 };
 
+// function was written in function declaration because of ES6 hoisting rules
+// logic was refactored to simplify
+// gets pageID -> checks redirectKey -> then proceeds in 1 of 3 paths
+// 1. null: no path found, return one of the redirectFailMessages
+// 2. redirectKey: recall wikiQuoteCall on the new redirectKey
+// 3. false: no redirect necessary, proceed to parsing.
 function getData(body, callback) {
   const pageID = getPageID(body);
   const redirect = redirectCheck(body, pageID);
@@ -138,6 +145,8 @@ function getData(body, callback) {
   return wikiQuoteCall(redirect, callback);
 }
 
+// frontEndCall handles the AJAX call from the front end.
+// modularized to be able to test better
 const frontEndCall = (req, res) => {
   const keyword = req.query.keyword;
   wikiQuoteCall(keyword, (response) => {
