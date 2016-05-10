8982fc7f4f58d5a08cdbd4d6b6d552aeab396232
Arun Vasudevan <arunv83@gmail.com>
2016-05-09 20:26:29 -0700

    Add auth.js file to create services for login and signup.

---
+++ client/app/auth/auth.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+
+const signup = (user, callback) => {
+  $.post('/api/users/signup', user, resp => {
+    callback(resp);
+  });
+};
+
+const login = (user, callback) => {
+  $.post('/api/users/login', user, resp => {
+    callback(resp);
+  });
+};
+
+export default {
+  signup,
+  login,
+};
