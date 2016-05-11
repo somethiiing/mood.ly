f6e4f706c6d1770b4ae1e02b502a0b9a1a509549
Arun Vasudevan <arunv83@gmail.com>
2016-05-11 16:28:43 -0700

    fixed merge conflict

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header from './components/header';
+import Header from './components/Header';
 import Footer from './components/footer';
 import LandingPage from './components/landing/landingPage';
 import Dashboard from './components/dashboard/dashboard';
