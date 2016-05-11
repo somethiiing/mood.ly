089225325eb14cd53067636e297bbeff16291e46
Wilson Yu <wilsonweichengyu@gmail.com>
2016-05-11 15:55:09 -0700

    Update music.js to include YouTube API and functions to make API calls. Tests in Node work and return video ID.

--- server/config/authConfig.js
+++ server/config/authConfig.js
@@ -9,4 +9,5 @@ export default {
     clientSecret: 'YOUR-GOOGLE-CLIENT-SECRET',
     callbackURL: 'http://localhost:8080/auth/google/callback',
   },
+  YOUTUBE_API_KEY: 'AIzaSyDjMlrErzW7nI9JkmF-R4z4wAhF9NmBNOU',
 };
