09d03a97fc540e4beb7e9dce9ddead6aacea3e41
Kim Curran <currankim7@gmail.com>
2016-05-13 11:57:04 -0700

    Reconfigure gifItem to use Material UI Card component, change Like button

--- client/app/components/dashboard/gifItem.js
+++ client/app/components/dashboard/gifItem.js
@@ -1,7 +1,11 @@
 import React, { PropTypes, Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Card, CardHeader, CardMedia } from 'material-ui/Card';
 import controller from '../../services/controllers';
+import ImageCamera from 'material-ui/svg-icons/image/camera-alt';
+import IconButton from 'material-ui/IconButton';
+import Favorite from 'material-ui/svg-icons/action/favorite';
 
 class GifItem extends Component {
   constructor(props) {
@@ -17,14 +21,24 @@ class GifItem extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
-          <img src={this.props.gif} alt="" />
-          <RaisedButton
-            label="like"
-            onClick={this.handleLikeButton}
-            primary={Boolean(true)}
-          />
-        </div>
+        <Card
+          style={{
+            height: 300,
+            width: 300,
+          }}
+        >
+          <CardHeader>
+            <IconButton><ImageCamera /></IconButton>
+            <IconButton
+              onClick={this.handleLikeButton}
+            >
+              <Favorite />
+            </IconButton>
+          </CardHeader>
+          <CardMedia>
+            <img src={this.props.gif} alt="" height="60%" width="70%" />
+          </CardMedia>
+        </Card>
       </MuiThemeProvider>
     );
   }
