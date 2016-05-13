d153b510ec1967c1053f508cd7eb14db814ee83e
Wilson Yu <wilsonweichengyu@gmail.com>
2016-05-13 12:24:06 -0700

    error handling in music file

--- server/API/music.js
+++ server/API/music.js
@@ -1,14 +1,8 @@
-// import request from 'request';
-// import key from '../config/authConfig';
-
-
-// FOR TESTING WITH NODE, can't use import
-const request = require('request');
-const key = require('youtubeconfig');
+import request from 'request';
+import key from '../config/authConfig';
 
 const parseData = (data) => {
   const result = [];
-
   for (let i = 0; i < data.length; i++) {
     result.push({
       title: data[i].title,
@@ -19,7 +13,7 @@ const parseData = (data) => {
   return result;
 };
 
-const songConverter = (songObj) => {
+const titleConverter = (songObj) => {
   const songTitle = songObj.title.split(' ');
   const songArtist = songObj.artist.split(' ');
   const song = songTitle.concat(songArtist);
@@ -31,7 +25,7 @@ const songConverter = (songObj) => {
   return result;
 };
 
-const searchYouTube = (song, callback) => {
+const searchYouTube = (song, trackInfo, callback) => {
   const youtubeURL = `
   https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${song}&type=video&videoEmbeddable=true&vq=hd1080&key=${key.YOUTUBE_API_KEY}`;
 
@@ -43,19 +37,29 @@ const searchYouTube = (song, callback) => {
       })
       .on('end', () => {
         body = JSON.parse(Buffer.concat(body).toString());
+        if (body.pageInfo.totalResults === 0) {
+          callback({ status: 'NOTFOUND', trackInfo, body: 'No videos found' });
+        }
         const youtubeArr = body.items;
         const randInd = Math.floor(Math.random() * youtubeArr.length);
-        callback(youtubeArr[randInd].id.videoId);
+        callback({ status: 'SUCCESS', trackInfo, videoID: youtubeArr[randInd].id.videoId });
       });
     });
 };
 
-const getYouTubeLink = (data, callback) => {
+const getYouTubeLink = (keyword, data, callback) => {
+  if (data === undefined) {
+    const keywordArr = keyword.split(' ');
+    let song = keyword[0];
+    for (let i = 1; i < keywordArr.length; i++) {
+      song += `+ ${keywordArr[i]}`;
+    }
+    return searchYouTube(song, keyword, callback);
+  }
   const songArr = parseData(data);
   const randInd = Math.floor(Math.random() * songArr.length);
-  const randSong = songConverter(songArr[randInd]);
-
-  searchYouTube(randSong, callback);
+  const song = songArr[randInd];
+  return searchYouTube(titleConverter(song), song, callback);
 };
 
 const musicoveryCall = (keyword, callback) => {
@@ -69,7 +73,7 @@ const musicoveryCall = (keyword, callback) => {
     .on('end', () => {
       body = JSON.parse(Buffer.concat(body).toString());
       // console.log('body ', body.root.tracks.track[0].artist[0].name);
-      getYouTubeLink(body.root.tracks.track, callback);
+      getYouTubeLink(keyword, body.root.tracks.track, callback);
     });
   });
 };
@@ -82,10 +86,10 @@ const frontEndCall = (req, res) => {
 };
 
 // TEST
-// musicoveryCall('happy', (data) => {
+// musicoveryCall('fantastic', (data) => {
 //   console.log(data);
 // });
 
-export default { parseData, songConverter,
+export default { parseData, titleConverter,
   searchYouTube, getYouTubeLink,
   musicoveryCall, frontEndCall };
