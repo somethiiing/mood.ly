9dfe798529aa1e051eef935db784955c8031efdb
Arun Vasudevan <arunv83@gmail.com>
2016-05-14 11:34:23 -0700

    Minor update in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,13 @@
 import Sequelize from 'sequelize';
 import dbConfig from '../config/mysqlsetup.js';
 
-export default new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  define: {
-    timestamps: false, // true by default
-  },
-  logging: false,
-});
+export default new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    define: {
+      timestamps: false, // true by default
+    },
+    logging: false,
+  });
