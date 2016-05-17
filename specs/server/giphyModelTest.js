34d5d6aa0381a1751d327dcae0f2add6b6faef94
Arun Vasudevan <arunv83@gmail.com>
2016-05-17 14:53:43 -0700

    Ongoing update to giphyModelTest.js

--- specs/server/giphyModelTest.js
+++ specs/server/giphyModelTest.js
@@ -6,9 +6,46 @@ import '../../server/index';
 const expect = chai.expect;
 
 describe('Giphy model', () => {
-  it('should create a new Giphy', (done) => {
+  // CREATE TEST DATABASE OF GIPHYS
+  const giphys = [
+    {
+      url: 'http://giphy.com/gifs/outlander-season-2-starz-l396CTgdPnnUx8L0Q',
+      mood: 'happy',
+    },
+    {
+      url: 'http://giphy.com/gifs/topgun-3o6EhQcCZmb1dMg3ug',
+      mood: 'sad',
+    },
+    {
+      url: 'http://giphy.com/gifs/3o7qDNOLb84uPBDCG4',
+      mood: 'excited',
+    },
+  ];
+
+  // CREATE USERS BEFORE EACH TEST
+  beforeEach(done => {
+    giphys.forEach(giphy => {
+      Giphy.findOrCreate({ where: giphy })
+      .then(() => {
+        // console.log('giphy==========> ', giphy);
+      })
+      .catch(err => {
+        console.log('Error! ', err);
+      });
+    });
+    done();
+  });
+  
+  it('should create a new Giphy', done => {
     const newGiphy = Giphy.create();
     expect(newGiphy).to.be.instanceOf(Object);
+    done();
+  });
+
+  it('should have a property of url', done => {
+    const newGiphy = Giphy.create();
+    console.log('newGiphy========> ', newGiphy);
+    expect(newGiphy).to.have.property('url');
     done();
   });
 });
