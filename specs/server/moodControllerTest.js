8297fdf6d3d36162ab7ba3ed4ebaeb8313c5cabe
Arun Vasudevan <arunv83@gmail.com>
2016-05-17 16:57:33 -0700

    Update to moodControllerTest.js (WIP)

--- specs/server/moodControllerTest.js
+++ specs/server/moodControllerTest.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+import { describe, it } from 'mocha';
 import request from 'request';
 import Mood from '../../server/models/moodModel';
 import User from '../../server/models/userModel';
@@ -6,54 +7,47 @@ import moodController from '../../server/controllers/moodController';
 const expect = chai.expect;
 
 describe('Mood Controller', () => {
-  var user = {
+  const user = {
     username: 'Toby',
     email: 'toby@toby.com',
-    password: '1234'
+    password: '1234',
   };
-  var mood1 = {
+  const mood1 = {
     name: 'happy',
-    timedate: new Date()
+    timedate: new Date(),
   };
-  var mood2 = {
+  const mood2 = {
     name: 'sad',
-    timedate: new Date()
+    timedate: new Date(),
   };
-  var mood3 = {
+  const mood3 = {
     name: 'ecstatic',
-    timedate: new Date()
+    timedate: new Date(),
   };
 
-  // beforeEach(done => {
-  //   User.findOrCreate({where: user})
-  //   .then(() => {
-  //     done();
-  //   });
-  // });
-
-  it('should add users mood to the database', done => {
+  it('should add user\'s mood to the database', done => {
     let options1 = {
       method: 'POST',
       uri: 'http://127.0.0.1:8080/api/moods',
       json: {
-        user: user,
+        user,
         mood: mood1,
       },
     };
-    
+
     let options2 = {
       method: 'GET',
       uri: 'http://127.0.0.1:8080/api/moods',
       json: {
-        user: user,
+        user,
       },
     };
 
-    User.findOrCreate({where: user})
+    User.findOrCreate({ where: user })
     .then(() => {
       request(options1, (err, res, body) => {
+        console.log('body=========> ', body);
         expect(res.statusCode).to.equal(201);
-    
 
         request(options2, (err, res, body) => {
           expect(res.statusCode).to.equal(200);
@@ -76,18 +70,20 @@ describe('Mood Controller', () => {
       method: 'POST',
       uri: 'http://127.0.0.1:8080/api/moods',
       json: {
-        user: user,
-        mood: mood2
-      }
-    }
+        user,
+        mood: mood2,
+      },
+    };
+
     let options3 = {
       method: 'POST',
       uri: 'http://127.0.0.1:8080/api/moods',
       json: {
-        user: user,
-        mood: mood3
-      }
-    }
+        user,
+        mood: mood3,
+      },
+    };
+
     request(options2, (err, res, body) => {
       expect(res.statusCode).to.equal(201);
       request(options3, (err, res, body) => {
@@ -102,14 +98,14 @@ describe('Mood Controller', () => {
       method: 'GET',
       uri: 'http://127.0.0.1:8080/api/moods',
       json: {
-        user: user
-      }
-    }
+        user,
+      },
+    };
+
     request(options, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       expect(body).to.have.length.above(1);
       done();
     });
   });
-
 });
