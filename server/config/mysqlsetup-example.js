925b50a3fa33739c140c946328580d3126d4dec3
Arun Vasudevan <arunv83@gmail.com>
2016-05-17 12:21:07 -0700

    Update to test TravisCI

--- server/config/mysqlsetup-example.js
+++ server/config/mysqlsetup-example.js
@@ -3,7 +3,7 @@ export default {
     driver: 'mysql',
     username: 'root', // TravisCI
     database: 'moodb',
-    password: 'HR41', // TravisCI
+    password: '', // TravisCI
     host: '127.0.0.1',
     port: '8080',
   },
