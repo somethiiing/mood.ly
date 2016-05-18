49418bfd41a4752191aa58ddbbd54381efd7cf21
Arun Vasudevan <arunv83@gmail.com>
2016-05-18 16:30:43 -0700

    Update package.json to add musicVideoControllerTest.js and update tests

--- specs/server/musicVideoControllerTest.js
+++ specs/server/musicVideoControllerTest.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+import { describe, it } from 'mocha';
 import request from 'request';
 import MusicVideo from '../../server/models/musicVideoModel';
 import User from '../../server/models/userModel';
@@ -6,21 +7,20 @@ import musicVideoController from '../../server/controllers/musicVideoController'
 const expect = chai.expect;
 
 describe('MusicVideo Controller', () => {
-
-  var user = {
+  const user = {
     username: 'Wilson',
     email: 'wilson@wilson.com',
     password: 'asdf',
   };
-  var musicVideo1 = {
+  const musicVideo1 = {
     url: 'http://media0.giphy.com/media/xThuWp2hJABbmc20Ew/200.gif',
     mood: 'Happy',
   };
-  var musicVideo2 = {
+  const musicVideo2 = {
     url: 'http://media1.giphy.com/media/xT5LMNrx71j8dZvJra/200.gif',
     mood: 'Sentimental',
   };
-  var musicVideo3 = {
+  const musicVideo3 = {
     url: 'http://media2.giphy.com/media/l4Ki7hFz3nr9PN1C0/200.gif',
     mood: 'Romantic',
   };
@@ -33,20 +33,20 @@ describe('MusicVideo Controller', () => {
   // });
 
   it('should add users musicVideo to the database', done => {
-    let options1 = {
+    const options1 = {
       method: 'POST',
       uri: 'http://127.0.0.1:8080/api/music',
       json: {
-        user: user,
+        user,
         musicVideo: musicVideo1,
       },
     };
 
-    let options2 = {
+    const options2 = {
       method: 'GET',
       uri: 'http://127.0.0.1:8080/api/music',
       json: {
-        user: user,
+        user,
       },
     };
 
@@ -72,19 +72,19 @@ describe('MusicVideo Controller', () => {
   });
 
   it('users should be able to have many musicVideos', done => {
-    let options2 = {
+    const options2 = {
       method: 'POST',
       uri: 'http://127.0.0.1:8080/api/music',
       json: {
-        user: user,
+        user,
         musicVideo: musicVideo2,
       },
     };
-    let options3 = {
+    const options3 = {
       method: 'POST',
       uri: 'http://127.0.0.1:8080/api/music',
       json: {
-        user: user,
+        user,
         musicVideo: musicVideo3,
       },
     };
@@ -98,11 +98,11 @@ describe('MusicVideo Controller', () => {
   });
 
   it('should get all user musicVideos', done => {
-    let options = {
+    const options = {
       method: 'GET',
       uri: 'http://127.0.0.1:8080/api/music',
       json: {
-        user: user,
+        user,
       },
     };
     request(options, (err, res, body) => {
