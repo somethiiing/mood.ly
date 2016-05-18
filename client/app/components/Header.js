392dfb2650c5697720e55c84b607b63badf621a8
Kim Curran <currankim7@gmail.com>
2016-05-18 11:11:56 -0700

    Remove AppBar from header component, configure new nav bar

--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import IconButton from 'material-ui/IconButton';
-import Menu from 'material-ui/svg-icons/navigation/menu';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import SocialPersonOutline from 'material-ui/svg-icons/social/person-outline';
-import Home from 'material-ui/svg-icons/action/home';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Button from 'react-bootstrap/lib/Button';
 
 class Header extends React.Component {
   constructor(props) {
@@ -36,63 +27,27 @@ class Header extends React.Component {
     this.props.handleMoodData();
   }
 
-  handleOpenMenu() {
-    this.setState({
-      openMenu: true,
-    });
-  }
-
-  handleOnRequestChange(value) {
-    this.setState({
-      openMenu: value,
-    });
-  }
-
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <AppBar
-          title="mood.ly"
-          style={{
-            backgroundColor: '#424242',
-            height: 65,
-          }}
-          showMenuIconButton={false}
-          iconElementRight={
-            <IconMenu
-              iconButtonElement={
-                <IconButton><Menu /></IconButton>
-              }
-              onChange={this.handleChangeSingle}
-              value={this.state.valueSingle}
-              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-            >
-              <MenuItem
-                value="1"
-                primaryText="dashboard"
-                onClick={this.handleMoodlyClick}
-                leftIcon={<Home />}
-              />
-              <MenuItem
-                value="2"
-                primaryText="profile"
-                onClick={this.handleProfileButtonClick}
-                leftIcon={<SocialPersonOutline />}
-              />
-              <MenuItem
-                value="3"
-                primaryText="logout"
-                onClick={this.handleLogoutButtonClick}
-                leftIcon={<NavigationClose />}
-              />
-            </IconMenu>
-          }
-          titleStyle={{
-            textAlign: 'left',
-          }}
-        />
-      </MuiThemeProvider>
+      <div className="nav-header container">
+        <h3 className="nav-title" onClick={this.handleOnMoodlyClick}>mood.ly</h3>
+        <div className="nav-buttons">
+          <Button
+            bsSize="large"
+            className="primary-button nav-button"
+            onClick={this.handleProfileButtonClick}
+          >
+          profile
+          </Button>
+          <Button
+            bsSize="large"
+            className="primary-button nav-button"
+            onClick={this.handleLogoutButtonClick}
+          >
+          logout
+          </Button>
+        </div>
+      </div>
     );
   }
 }
