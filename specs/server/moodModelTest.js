2557f91aba90890349f5773fbf090331721abd1d
Arun Vasudevan <arunv83@gmail.com>
2016-05-18 23:23:10 -0700

    Add tests in moodModelTest.js to check data persistence - all tests pass.

--- specs/server/moodModelTest.js
+++ specs/server/moodModelTest.js
@@ -1,14 +1,28 @@
 import chai from 'chai';
 import { describe, it } from 'mocha';
+import request from 'request';
 import Mood from '../../server/models/moodModel';
 import '../../server/index';
 
 const expect = chai.expect;
 
 describe('Mood model', () => {
-  it('should create a new mood', done => {
-    const newMood = Mood.create();
-    expect(newMood).to.be.instanceOf(Object);
+  const moodFetch = {
+    method: 'GET',
+    uri: 'http://127.0.0.1:8080/api/moods',
+  };
+
+  it('should have a name property', done => {
     done();
+    request(moodFetch, (err, res, body) => {
+      expect(JSON.parse(body)[0]).to.have.property('name');
+    });
+  });
+
+  it('should return a status code of 200', done => {
+    request(moodFetch, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
 });
