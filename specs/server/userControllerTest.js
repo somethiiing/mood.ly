5c56d5d086b4cc677ee3c1073702a1a46bfd270a
Arun Vasudevan <arunv83@gmail.com>
2016-05-18 23:32:18 -0700

    Add tests to userControllerTest.js to check data persistence and retrieval - all tests pass.

--- specs/server/userControllerTest.js
+++ specs/server/userControllerTest.js
@@ -1,94 +1,68 @@
 import chai from 'chai';
+import { describe, it } from 'mocha';
 import request from 'request';
-import User from '../../server/models/userModel';
-import userController from '../../server/controllers/userController';
+
 const expect = chai.expect;
 
 describe('User Controller', () => {
-  //CREATE NEW DB FOR TESTING
-  var users = [
-    {
-      name: 'Magee',
-      email: 'magee@magee.com',
-      password: '1234',
-    },
-    {
-      name: 'Dan',
-      email: 'dan@dan.com',
-      password: '4321',
-    },
-    {
-      name: 'Beth',
-      email: 'beth@beth.com',
-      password: '5678',
-    },
-    {
-      name: 'Sunny',
-      email: 'sunny@sunny.com',
-      password: '8765',
-    },
-    {
-      name: 'Zach',
-      email: 'zach@zach.com',
-      password: '7890',
-    }
-  ];
-
-  //CREATE USERS AND POPULATE DATABASE
-  beforeEach((done) => {
-    User.findOrCreate({ where: users })
-    .then(() => {
-      done();
-    });
-  });
-
-  //RETRIEVE ALL
-  it('should have a method that retrieves all users from the database', (done) => {
-    let options = {
+  // RETRIEVE ALL
+  it('should have a method that retrieves all users from the database', done => {
+    const options = {
       method: 'GET',
       uri: 'http://127.0.0.1:8080/api/users',
-      json: {
-        users: users
-      }
     };
 
-    request(options, (err, user, body) => {
+    request(options, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
-      expect(body.length).to.equal(5);
+      // console.log('body (userController)==========> ', body);
+      expect(JSON.parse(body).length).to.equal(5);
       done();
     });
   });
 
-  it('should update a user in the database given new information', (done) => {
-    let options = {
+  it('should have a property of username', done => {
+    const options = {
+      method: 'GET',
+      uri: 'http://127.0.0.1:8080/api/users',
+    };
+
+    request(options, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(JSON.parse(body)[0]).to.have.property('username');
+      done();
+    });
+  });
+
+  it('should update a user in the database given new information', done => {
+    const options = {
       method: 'PUT',
       uri: 'http://127.0.0.1:8080/api/users',
       json: {
-        //UPDATED INFO
-      }
+        name: 'Toby',
+        username: 'Toby the blockhead',
+        email: 'toby@blockhead.com',
+      },
     };
 
-    request(options, (err, user, body) => {
+    request(options, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
-      expect().to.equal(); //NEED TO COMPLETE
+      console.log('res.request.body (PUT)==========> ', res.request.body);
+      expect(res.request.body.email).to.equal('toby@blockhead.com');
       done();
     });
   });
 
-  it('should delete a user from the database', (done) => {
-    let options = {
+  it('should delete a user from the database', done => {
+    const options = {
       method: 'DELETE',
       uri: 'http://127.0.0.1:8080/api/users',
-      json: {
-        users: users
-      }
     };
 
-    request(options, (err, user, body) => {
+    request(options, (err, res, body) => {
+      // console.log('body (DELETE)==========> ', body);
       expect(res.statusCode).to.equal(200);
       expect(body.length).to.equal(4);
       done();
     });
   });
-
 });
