e1e7f3f40da33f636beb9005e010b15d6e7eea1e
Arun Vasudevan <arunv83@gmail.com>
2016-05-19 15:41:31 -0700

    Change database to 'moodlyTest'

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,12 @@
 import Sequelize from 'sequelize';
-import Config from '../config/mysqlsetup-example.js';
+import Config from '../config/mysqlsetup.js';
 
 export default new Sequelize(
-  // Config.dbConfig.database,
-  'moodb',
-  // Config.dbConfig.username,
-  'travis',
-  // Config.dbConfig.password, // FOR TRAVISCI TESTING - USUALLY IS 'HR41'
+  Config.dbConfig.database,
+  // 'moodb',
+  Config.dbConfig.username,
+  // 'travis',
+  Config.dbConfig.password, // FOR TRAVISCI TESTING - USUALLY IS 'HR41'
   '',
   {
     host: Config.dbConfig.host,
