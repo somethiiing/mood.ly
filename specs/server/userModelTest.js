33c4a368d31507b6c16f8e86efe402ac15cadf7d
Arun Vasudevan <arunv83@gmail.com>
2016-05-19 00:27:50 -0700

    Add tests to userModelTest.js to check data persistence and retrieval - all tests pass.

--- specs/server/userModelTest.js
+++ specs/server/userModelTest.js
@@ -1,13 +1,34 @@
 import chai from 'chai';
-import User from '../../server/models/userModel';
+import { describe, it } from 'mocha';
+import request from 'request';
 import '../../server/index';
+
 const expect = chai.expect;
 
-describe('User model', function() {
+describe('User model', () => {
+  const userFetch = {
+    method: 'GET',
+    uri: 'http://127.0.0.1:8080/api/users',
+  };
 
-  it('should create a new User', () => {
-    let newUser = User.create();
-    expect(newUser).to.be.instanceOf(Object);
+  it('should match the username property of the specified user', done => {
+    done();
+    request(userFetch, (err, res, body) => {
+      expect(JSON.parse(body)[1].username).to.equal('Stephanie');
+    });
   });
 
+  it('should return a status code of 200', done => {
+    request(userFetch, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the number of users in the database', done => {
+    request(userFetch, (err, res, body) => {
+      expect(JSON.parse(body).length).to.equal(5);
+      done();
+    });
+  });
 });
