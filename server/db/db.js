d5876abc3c9ec2ee84d8c6036fc215422b1378d6
Arun Vasudevan <arunv83@gmail.com>
2016-05-19 16:14:01 -0700

    Hardcoded info to test TravisCI

--- server/db/db.js
+++ server/db/db.js
@@ -2,14 +2,14 @@ import Sequelize from 'sequelize';
 import Config from '../config/mysqlsetup.js';
 
 export default new Sequelize(
-  Config.dbConfig.database,
-  // 'moodb',
-  Config.dbConfig.username,
-  // 'travis',
-  Config.dbConfig.password, // FOR TRAVISCI TESTING - USUALLY IS 'HR41'
+  // Config.dbConfig.database,
+  'moodb',
+  // Config.dbConfig.username,
+  'root',
+  // Config.dbConfig.password, // FOR TRAVISCI TESTING - USUALLY IS 'HR41'
   '',
   {
-    host: Config.dbConfig.host,
+    host: 'http://127.0.0.1', // Config.dbConfig.host,
     dialect: 'mysql',
     define: {
       timestamps: false, // true by default
