2384a8abda32d36f4d1aa75ad60f0432ce41921d
Wilson Yu <wilsonweichengyu@gmail.com>
2016-05-20 10:41:29 -0700

    revert w/o infinite scroll

--- client/app/components/dashboard/dashboard.js
+++ client/app/components/dashboard/dashboard.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Music from './music';
 import Search from './search';
+import GifItem from './gifItem';
+import QuoteItem from './quoteItem';
 import Dialog from 'material-ui/Dialog';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
 import services from '../../services/services';
 import Button from 'react-bootstrap/lib/Button';
 import controller from '../../services/controllers';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import GridView from './gridView';
-import Infinite from 'react-infinite';
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -29,8 +33,6 @@ class Dashboard extends React.Component {
     };
 
     this.apiCalls = this.apiCalls.bind(this);
-    this.buildElements = this.buildElements.bind(this);
-    this.handleInfiniteLoad = this.handleInfiniteLoad.bind(this);
     this.emptyCheck = this.emptyCheck.bind(this);
     this.dialogOpen = this.dialogOpen.bind(this);
     this.dialogClose = this.dialogClose.bind(this);
@@ -106,9 +108,6 @@ class Dashboard extends React.Component {
 
   handleSearchButtonClick() {
     const query = this.state.currSearch;
-    this.setState({
-      elements: [],
-    });
     if (this.emptyCheck(query) === false) {
       this.apiCalls();
       controller.addUserMood(query, this.props.user, (res) => { console.log(res); });
@@ -116,7 +115,6 @@ class Dashboard extends React.Component {
     if (this.emptyCheck(query) === true) {
       this.dialogOpen();
       this.setState({
-        isInfiniteLoading: false,
         currMood: ' ',
         currSearch: ' ',
       });
@@ -129,59 +127,7 @@ class Dashboard extends React.Component {
     });
   }
 
-  buildElements() {
-    // this.handleSearchButtonClick();
-    const elements = [];
-    elements.push(
-      <GridView
-        showGifItem={this.state.showGifItem}
-        showQuoteItem={this.state.showQuoteItem}
-        showMusicItem={this.state.showMusicItem}
-        currentGif={this.state.currentGif}
-        currMood={this.state.currMood}
-        user={this.props.user}
-        currQuote={this.state.currQuote}
-        currVideoID={this.state.currVideoID}
-      />
-    );
-    return elements;
-  }
-
-  handleInfiniteLoad() {
-    if (this.emptyCheck(this.state.currSearch) === false) {
-      this.apiCalls();
-    }
-    console.log('handle on infinite load');
-    this.setState({
-      isInfiniteLoading: true,
-    });
-    setTimeout(() => {
-      const newElements = this.buildElements();
-      this.setState({
-        isInfiniteLoading: false,
-        elements: this.state.elements.concat(newElements),
-      });
-    }, 2000);
-  }
-  elementInfiniteLoad() {
-    // PLACE TO  PUT A LOADING ELEMENT
-    console.log('element infinite load');
-    return (
-      <div className="infinite-list-item">
-        <img
-          src="/client/app/styles/spinner-blue.gif"
-          style={{
-            width: 40,
-            height: 40,
-            'margin-left': '49vw',
-          }}
-        />
-      </div>
-    );
-  }
-
   render() {
-
     const actions = [
       <Button
         bsSize="large"
@@ -207,17 +153,36 @@ class Dashboard extends React.Component {
           Oops. Please enter how you're feeling then hit submit!
           </Dialog>
         </MuiThemeProvider>
-        <Infinite
-          elementHeight={500}
-          infiniteLoadBeginEdgeOffset={1000}
-          useWindowAsScrollContainer={true}
-          onInfiniteLoad={this.handleInfiniteLoad}
-          loadingSpinnerDelegate={this.elementInfiniteLoad()}
-          isInfiniteLoading={this.state.isInfiniteLoading}
-          timeScrollStateLastsForAfterUserScrolls={2500}
-        >
-          {this.state.elements}
-        </Infinite>
+        <Grid>
+          <Row
+            className="show-grid"
+            style={{
+              paddingBottom: 100,
+            }}
+          >
+            {this.state.showGifItem ?
+              <Col sm={6} md={4} className="card-spacing">
+                <GifItem
+                  gif={this.state.currentGif}
+                  mood={this.state.currMood}
+                  user={this.props.user}
+                /></Col> : null}
+            {this.state.showQuoteItem ?
+              <Col sm={6} md={4} className="card-spacing">
+                <QuoteItem
+                  quote={this.state.currQuote}
+                  mood={this.state.currMood}
+                  user={this.props.user}
+                /></Col> : null}
+            {this.state.showMusicItem ?
+              <Col sm={6} md={4} className="card-spacing">
+                <Music
+                  videoId={this.state.currVideoID}
+                  mood={this.state.currMood}
+                  user={this.props.user}
+                /></Col> : null}
+          </Row>
+        </Grid>
       </div>
     );
   }
