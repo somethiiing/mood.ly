f135175ec2579a7c483460030e630d22aee16248
Arun Vasudevan <arunv83@gmail.com>
2016-05-19 19:39:34 -0700

    Update .travis.yml with environment variables and set vars in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,24 @@
 import Sequelize from 'sequelize';
 import Config from '../config/mysqlsetup.js';
 
+const travisCI = {
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+};
+
+if (!process.env.TRAVIS) {
+  travisCI.username = Config.dbConfig.username;
+  travisCI.password = Config.dbConfig.password;
+  travisCI.database = Config.dbConfig.database;
+}
 export default new Sequelize(
-  // Config.dbConfig.database,
-  'moodb',
-  // Config.dbConfig.username,
-  'root',
-  // Config.dbConfig.password, // FOR TRAVISCI TESTING - USUALLY IS 'HR41'
-  '',
+  travisCI.database,
+  // 'moodb',
+  travisCI.username,
+  // 'root',
+  travisCI.password, // FOR TRAVISCI TESTING - USUALLY IS 'HR41'
+  // '',
   {
     // host: 'http://127.0.0.1', // Config.dbConfig.host,
     dialect: 'mysql',
